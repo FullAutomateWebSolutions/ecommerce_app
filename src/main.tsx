f5edd9766bc1c18107248ee8403545f482219973
alex-Sandro-alves-de-lima <lexjustin5@gmail.com>
2025-07-11 17:10:25 -0300

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       <ConfigProvider locale={ptBR}>
         <AntdApp> 
           <AuthProvider>
